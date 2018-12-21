800db578d933d7a45631fc9d1c4bcdb5461f992f
sky3141 <18201117753@163.com>
2018-12-21 18:22:21 +0800

    change BTN_DIFFICULTY(0x0833 -> 0x0001)

--- PuzzleGame/window.h
+++ PuzzleGame/window.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define BTN_DIFFICULTY	    0x0833
+#define BTN_DIFFICULTY	    0x0001
 #define BTN_RANDOM          0x0002
 #define BTN_SOLVE           0x0003
 #define BTN_AUTO            0x0004
